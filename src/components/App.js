8df6750aeb9791e727ba3320e3f74fb6a912ae47
LornahMunene <munenelornah9@gmail.com>
2024-04-27 15:07:18 +0300

    import-export-lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import NavBar from "../components/NavBar"
+import Home from "../components/Home"
+import About from "../components/About"
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
       <About />
+      
     </div>
   );
 }
